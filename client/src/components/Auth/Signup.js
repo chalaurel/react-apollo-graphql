06ef05536bcd947f164bb0942896be0b5f5e3891
chalaurel <cm_laurel@yahoo.com>
2019-02-13 23:21:24 +0800

    signup mutation

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,17 +1,47 @@
 import React from 'react';
-
+import { Mutation } from 'react-apollo';
+import { SIGNUP_USER } from '../../queries';
 class Signup extends React.Component {
+
+    state = {
+        username: "",
+        email: "",
+        password: "",
+        passwordConfirmation: ""
+    }
+    handleChange = event => {
+        const { name, value } = event.target;
+
+        this.setState({ [name]: value });
+    }
+    handleSubmit = (event, signupUser) => {
+        event.preventDefault();
+
+        signupUser().then(data => {
+            console.log(data);
+        })
+    }
+
     render() {
+        const { username, email, password, passwordConfirmation } = this.state;
         return (
             <div className="App">
                 <h1>Signup</h1>
-                <form className="form">
-                    <input type="text" name="username" placeholder="Username" />
-                    <input type="email" name="email" placeholder="E-mail" />
-                    <input type="password" name="password" placeholder="Password" />
-                    <input type="password" name="passwordConfirmation" placeholder="Password Confirm" />
-                    <button type="submit">Submit</button>
-                </form>
+                <Mutation mutation={SIGNUP_USER} variables={{ username, email, password }}>
+                    {(signupUser, { data, loading, error }) => {
+
+                        return (
+                            <form className="form" onSubmit={event => this.handleSubmit(event, signupUser)}>
+                                <input type="text" name="username" placeholder="Username" value={username} onChange={this.handleChange} />
+                                <input type="email" name="email" placeholder="E-mail" value={email} onChange={this.handleChange} />
+                                <input type="password" name="password" placeholder="Password" value={password} onChange={this.handleChange} />
+                                <input type="password" name="passwordConfirmation" placeholder="Password Confirm" value={passwordConfirmation} onChange={this.handleChange} />
+                                <button type="submit">Submit</button>
+                            </form>
+                        )
+                    }}
+
+                </Mutation>
             </div>
         )
     }
--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -1,5 +1,6 @@
 import { gql } from 'apollo-boost';
 
+/* Recipe Queries */
 export const GET_ALL_RECIPES = gql`
   query {
     getAllRecipes {
@@ -11,6 +12,20 @@ export const GET_ALL_RECIPES = gql`
       createdDate
       likes
       username
+    }
+  }
+`;
+
+/* Recipe Mutations */
+
+/** User Queries */
+
+/** User Mutations */
+
+export const SIGNUP_USER = gql`
+  mutation ($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
     }
   }
 `;
